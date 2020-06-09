8226db1853cb7223ebbf9369d698a3553f875fb9
ssunghwan <ssunghwan1@gmail.com>
2020-06-09 13:53:48 +0900

    Test commit

---
+++ src/views/NewsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+      <!-- <div v-for ="user in users">{{user}}</div> -->
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import
+export default {
+  data(){
+    return {
+      users:[]
+    }
+  },
+  created(){
+    var vm = this;
+    axios.get('https://api.hnpwa.com/v0/news/1.json')
+    .then(function(response){
+      console.log(response);
+      vm.users = response.data;
+    })
+    .catch(function(error){
+      console.log(error)
+    })
+  },
+}
+</script>
+
+<style>
+
+</style>
